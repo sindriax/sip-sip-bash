3ff9826ac607e19e407f107ae1b42ac5d57e8a35
Sandra Ramirez Puente <sandra.ramirez@acaitravel.com>
2024-11-17 11:42:01 +0100

    enhance card UI with hover effects and layout adjustments

--- src/utils/game-rules.ts
+++ src/utils/game-rules.ts
@@ -1,9 +1,20 @@
-export const GameRules: { [key: string]: string } = {
+interface GameRule {
+    [key: string]: string;
+}
+
+export const GameRules: GameRule = {
     "Ace of Spades":
-      "Waterfall: Everyone starts drinking and can’t stop until the person to their right stops.",
+        "Waterfall: Everyone starts drinking and can’t stop until the person to their right stops.",
     "2 of Hearts": "Choose someone to take a drink.",
     "3 of Diamonds": "You take a drink.",
     "4 of Spades": "Create a new rule.",
     "5 of Clubs": "Everyone drinks!",
-  };
+    "6 of Hearts": "Six is for chicks",
+    "7 of Diamonds": "Seven is for heaven",
+    "8 of Clubs": "Eight is for mate",
+    "9 of Spades": "Nine is for rhyme",
+    "10 of Hearts": "Categories",
+    "Queen of Clubs": "Queen is for questions",
+    "King of Spades": "King is for rule",
+};
   
